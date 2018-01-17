bccd414b36c2d595cc50ed48a749ab67e9d79202
Alexandre Payette <apayette@videotron.ca>
2018-01-16 21:13:03 -0500

    Create app-routing.module + update app.module
    Import all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { WebDesignComponent } from './pages/web-design/web-design.component';
+import { AnimationComponent } from './pages/animation/animation.component';
+import { PhotoComponent } from './pages/photo/photo.component';
+import { TechnicalSkillsComponent } from './pages/technical-skills/technical-skills.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderNavComponent
+    HeaderNavComponent,
+    WebDesignComponent,
+    AnimationComponent,
+    PhotoComponent,
+    TechnicalSkillsComponent,
+    ExperienceComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
