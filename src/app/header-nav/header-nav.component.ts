c7fd2d08e3f3e0777ae9197ae702539abf8c3b8d
Alexandre Payette <apayette@videotron.ca>
2018-01-20 14:10:39 -0500

    add/header-nav.ts file - code styling

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-nav',
   templateUrl: './header-nav.component.html',
   styleUrls: ['./header-nav.component.scss']
 })
-export class HeaderNavComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HeaderNavComponent {}
