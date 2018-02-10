f12e60502e324fdec892eb3affae059c9ab341e0
Alexandre Payette <apayette@videotron.ca>
2018-02-10 14:23:44 -0500

    Create WebProject interface

---
+++ src/app/pages/web-design/web-design-project.model.ts
@@ -0,0 +1,11 @@
+export interface WebProject {
+  Category: string;
+  Color: string;
+  Date: string;
+  Descrip_En: string;
+  Descrip_Fr: string;
+  Id: string;
+  Order: number;
+  Title_En: string;
+  Title_Fr: string;
+}
