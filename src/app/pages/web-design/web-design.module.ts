f59ec91daf30b1a439422eba7d79958872b495a4
Alexandre Payette <apayette@videotron.ca>
2018-02-21 19:44:50 -0500

    Create a custum sort pipe | add it to web-design module

--- src/app/pages/web-design/web-design.module.ts
+++ src/app/pages/web-design/web-design.module.ts
@@ -4,11 +4,13 @@ import { NgSelectModule, NG_SELECT_DEFAULT_CONFIG } from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 import { WebDesignComponent } from './web-design.component';
 import { WebDesignItemComponent } from './web-design-item/web-design-item.component';
+import { ArraySortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
     WebDesignComponent,
-    WebDesignItemComponent
+    WebDesignItemComponent,
+    ArraySortPipe
   ],
   imports: [
     CommonModule,
