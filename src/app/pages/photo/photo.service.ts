e5644a63bd859fc7d082b70860bab5cffb220fb6
Alexandre Payette <apayette@videotron.ca>
2018-03-24 11:50:37 -0400

    PhotoService: Use Pipeable Operators and map. Object property as  parameter

--- src/app/pages/photo/photo.service.ts
+++ src/app/pages/photo/photo.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { PhotoItem } from './photo-item.model';
 import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoService {
@@ -9,19 +11,31 @@ export class PhotoService {
 
   getPhotos(tags: string, photoFormat: string, prePage: number, pageNumber: number): Observable<any> {
     const photoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search'
-    + '&api_key=891afde0528d42592ceb04c5c7cef64f'
-    + '&user_id=7820426%40N03&'
-    + 'tags='
-    + tags
-    + '&content_type=1&'
-    + 'extras='
-    + photoFormat
-    + '&per_page='
-    + prePage
-    + '&page='
-    + pageNumber
-    + '&format=json&nojsoncallback=1';
+      + '&api_key=891afde0528d42592ceb04c5c7cef64f'
+      + '&user_id=7820426%40N03&'
+      + 'tags='
+      + tags
+      + '&content_type=1&'
+      + 'extras='
+      + photoFormat
+      + '&per_page='
+      + prePage
+      + '&page='
+      + pageNumber
+      + '&format=json&nojsoncallback=1';
 
-    return this.http.get<any>(photoUrl);
+    return this.http.get<any>(photoUrl)
+      .pipe(
+        map(res => {
+            return res.photos.photo
+              .map(item => {
+                return {
+                  title: item.title,
+                  url: item[photoFormat]
+                } as PhotoItem;
+              });
+          }
+        )
+      );
   }
 }
