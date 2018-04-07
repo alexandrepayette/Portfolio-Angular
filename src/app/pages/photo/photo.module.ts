0fb066d7be51283b635ff4f7639a59e072fb172c
Alexandre Payette <apayette@videotron.ca>
2018-04-07 10:15:27 -0400

    PhotoModule - code styling

--- src/app/pages/photo/photo.module.ts
+++ src/app/pages/photo/photo.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo.component';
-import { HttpCacheService } from "../../shared/http-cache.service";
-import { CacheInterceptor } from "../../shared/cache.interceptor";
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpCacheService } from '../../shared/http-cache.service';
+import { CacheInterceptor } from '../../shared/cache.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
