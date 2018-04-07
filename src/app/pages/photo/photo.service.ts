c966b11b3b881f77f022e79f4ce2c79c8e6b8324
Alexandre Payette <apayette@videotron.ca>
2018-04-07 10:17:52 -0400

    PhotoService- tap - get error from flickr api / ES6 Template literals

--- src/app/pages/photo/photo.service.ts
+++ src/app/pages/photo/photo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PhotoItem } from './photo-item.model';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable()
 export class PhotoService {
@@ -10,27 +10,28 @@ export class PhotoService {
   constructor(private http: HttpClient) { }
 
   private photoUrl: string;
+  public errorMessage= 'noError';
 
   getPhotoUrl (tags: string, photoFormat: string, perPage: number, pageNumber?: number) {
-    this.photoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search'
-      + '&api_key=891afde0528d42592ceb04c5c7cef64f'
-      + '&user_id=7820426%40N03&'
-      + 'tags='
-      + tags
-      + '&content_type=1&'
-      + 'extras='
-      + photoFormat
-      + '&per_page='
-      + perPage
-      + '&page='
-      + pageNumber
-      + '&format=json&nojsoncallback=1';
+    this.photoUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search\
+&api_key=891afde0528d42592ceb04c5c7cef64f&user_id=7820426%40N03&\
+tags=${tags}\
+&content_type=1\
+&extras=${photoFormat}\
+&per_page=${perPage}\
+&page=${pageNumber}\
+&format=json&nojsoncallback=1`;
   }
 
   getPhotosArray(tags: string, photoFormat: string, perPage: number, pageNumber: number): Observable<any> {
     this.getPhotoUrl(tags, photoFormat, perPage, pageNumber);
     return this.http.get<any>(this.photoUrl)
       .pipe(
+        tap(res => {
+          if (res.stat === 'fail') {
+            this.errorMessage = res.message;
+          }
+        }),
         map(res => {
             return res.photos.photo
               .map(item => {
@@ -50,5 +51,9 @@ export class PhotoService {
       .pipe(
         map(res => Array.from({length: res.photos.pages}, (x, i) => i + 1))
       );
+  }
+
+  getErrorMessage() {
+    return this.errorMessage;
   }
 }
