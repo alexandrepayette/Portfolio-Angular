8827486237b0476df37cd795c0bf231c08956397
Alexandre Payette <apayette@videotron.ca>
2018-04-07 21:46:00 -0400

    Photo.component:  subscribe to number of pages add logic + Unsubscribe
    If the actual page is greater than the actual number of pages, the page number must be equal to the number of pages

--- src/app/pages/photo/photo.component.ts
+++ src/app/pages/photo/photo.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { PhotoService } from './photo.service';
 import { PhotoItem } from './photo-item.model';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import { catchError } from 'rxjs/operators';
+import { catchError, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
   styleUrls: ['./photo.component.scss']
 })
-export class PhotoComponent implements OnInit {
+export class PhotoComponent implements OnInit, OnDestroy {
 
   public photosObservable: Observable<PhotoItem[]>;
   public numberOfPagesObservable: Observable<Array<number>>;
@@ -22,6 +23,8 @@ export class PhotoComponent implements OnInit {
   // url_s = 160x240, url_t = 66x100, url_q = 150x150, url_m = 333x500
   private photoFormat = 'url_s';
   private perPage = 20;
+  private numberOfPages: number;
+  private ngUnsubscribe: Subject = new Subject();
 
   constructor(private photoService: PhotoService) { }
 
@@ -49,8 +52,6 @@ export class PhotoComponent implements OnInit {
           return Observable.throw(err);
         })
       );
-
-    // this.photosObservable.subscribe(res => { console.log(res); });
   }
 
   getErrorMessage() {
@@ -71,6 +72,23 @@ export class PhotoComponent implements OnInit {
     this.tags = orientation;
     this.perPage = orientation === 'best' ? 20 : 21;
     this.getNumberOfPages();
-    this.displayPhotos(this.pageToDisplay);
+
+    this.numberOfPagesObservable
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+      res => this.numberOfPages = res.length,
+      err => console.error('Can not get the number of pages: ' + err),
+      () => {
+        if (this.pageToDisplay > this.numberOfPages) {
+          this.pageToDisplay = this.numberOfPages;
+        }
+        this.displayPhotos(this.pageToDisplay);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
