ee87a1a7c98ef5448971db95535fedb7365c2b61
Alexandre Payette <apayette@videotron.ca>
2018-04-21 19:48:03 -0400

    Photo.module: import UICarouselModule

--- src/app/pages/photo/photo.module.ts
+++ src/app/pages/photo/photo.module.ts
@@ -4,13 +4,15 @@ import { PhotoComponent } from './photo.component';
 import { HttpCacheService } from '../../shared/http-cache.service';
 import { CacheInterceptor } from '../../shared/cache.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UICarouselModule } from 'ui-carousel';
 
 @NgModule({
   declarations: [
     PhotoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UICarouselModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true},
