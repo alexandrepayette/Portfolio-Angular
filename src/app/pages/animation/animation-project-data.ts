546df3f71181467934bc8edcf16ee60df5548c32
Alexandre Payette <apayette@videotron.ca>
2018-04-29 10:23:43 -0400

    Animation project data & model

---
+++ src/app/pages/animation/animation-project-data.ts
@@ -0,0 +1,154 @@
+import { AnimationProject } from './animation-project.model';
+
+export const ANIMATIONPROJECTS: AnimationProject[] = [
+  {
+    imageUrl: 'union-force',
+    title: 'L\'union fait la force, Radio-Canada',
+    date: 2003,
+    color: 'rouge'
+  },
+  {
+    imageUrl: 'auberge-chien-noir',
+    title: 'Auberge du Chien Noir, Radio-Canada',
+    date: 2003,
+    color: 'gris'
+  },
+  {
+    imageUrl: 'imax',
+    title: 'IMAX',
+    date: 1998,
+    color: 'brun'
+  },
+  {
+    imageUrl: 'soule-ton-boss',
+    title: 'Soul ton Boss, Cyber Generation',
+    date: 2002,
+    color: 'rouge'
+  },
+  {
+    imageUrl: 'animation-flash',
+    title: 'Animation Flash',
+    date: 2008,
+    color: 'mauve'
+  },
+  {
+    imageUrl: 'jeux-flash',
+    title: 'Jeux Flash',
+    date: 2008,
+    color: 'brun'
+  },
+  {
+    imageUrl: 'portfolio',
+    title: 'Portfolio',
+    date: 2008,
+    color: 'bleu'
+  },
+  {
+    imageUrl: 'on-ne-ment-que-deux-fois',
+    title: 'On ne ment que deux fois, Radio-Canada',
+    date: 2003,
+    color: 'vert'
+  },
+  {
+    imageUrl: 'mikado',
+    title: 'MIKADO bikes, Macromedia site of the day',
+    date: 2002,
+    color: 'orange'
+  },
+  {
+    imageUrl: 'porte-jpr',
+    title: 'Porte JPR Web Site',
+    date: 2003,
+    color: 'bleu'
+  },
+  {
+    imageUrl: 'centre-science',
+    title: 'Centre des sciences de Montréal',
+    date: 2000,
+    color: 'rouge'
+  },
+  {
+    imageUrl: 'fraser-contract',
+    title: 'Fraser Contract web site',
+    date: 2002,
+    color: 'gris'
+  },
+  {
+    imageUrl: 'dressing-room',
+    title: 'Dressing Room application MVM',
+    date: 2008,
+    color: 'rose'
+  },
+  {
+    imageUrl: 'print',
+    title: 'Print projects',
+    date: 2008,
+    color: 'rose'
+  },
+  {
+    imageUrl: 'hm',
+    title: 'H&M application prototype',
+    date: 2007,
+    color: 'bleu'
+  },
+  {
+    imageUrl: 'msn',
+    title: 'MSN Dressing room application',
+    date: 2007,
+    color: 'vert'
+  },
+  {
+    imageUrl: 'raid',
+    title: 'Raid Web Site',
+    date: 2001,
+    color: 'orange'
+  },
+  {
+    imageUrl: 'oryx-evolution',
+    title: 'ORYX Bikes application',
+    date: 2004,
+    color: 'gris'
+  },
+  {
+    imageUrl: 'reebok',
+    title: 'Reebok Prototype',
+    date: 2007,
+    color: 'gris'
+  },
+  {
+    imageUrl: 'carte-noel-mvm',
+    title: 'Chrismas card MVM',
+    date: 2005,
+    color: 'bleu'
+  },
+  {
+    imageUrl: 'cerveau-direction',
+    title: 'Cerveau Direction, Radio-Canada',
+    date: 2000,
+    color: 'bleu'
+  },
+  {
+    imageUrl: 'renovation-ram',
+    title: 'Renovation Ram Web Site',
+    date: 2003,
+    color: 'orange'
+  },
+  {
+    imageUrl: 'oryx-evolution-builder',
+    title: 'ORYX Bikes application',
+    date: 2003,
+    color: 'rouge'
+  },
+  {
+    imageUrl: 'radio-canada-50',
+    title: 'Radio-Canada 50e anniversaire',
+    date: 2002,
+    color: 'gris'
+  },
+  {
+    imageUrl: 'sears-usa',
+    title: 'Sears Application prototype',
+    date: 2008,
+    color: 'bleu'
+  }
+];
