dea5ba28860e028916945ba155075bef7e04acde
Alexandre Payette <apayette@videotron.ca>
2018-05-01 21:25:03 -0400

    WebDesignItem: ViewEncapsulation.None / HostBinding d-block

--- src/app/pages/web-design/web-design-item/web-design-item.component.ts
+++ src/app/pages/web-design/web-design-item/web-design-item.component.ts
@@ -1,13 +1,18 @@
-import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {Component, HostListener, HostBinding, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 import { WebProject } from '../web-design-project.model';
 
+// ViewEncapsulation.None -> can not use :host {} in the scss file!
+// Should use @HostBinding('class')...
 @Component({
   selector: 'app-web-design-item',
   templateUrl: './web-design-item.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./web-design-item.component.scss']
 })
 export class WebDesignItemComponent implements OnInit {
+  @HostBinding('class') displayBlockClass = 'd-block';
+
   @Input() inputProject: WebProject;
 
   imageUrl = 'url(http://acidvoice.com/portfolio_angular/assets/img/web_design_thumb/';
