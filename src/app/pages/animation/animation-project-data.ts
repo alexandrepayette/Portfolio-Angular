80d4a8b4f9376d838482f3c28fb6e73ad3a3ed37
Alexandre Payette <apayette@videotron.ca>
2018-05-20 10:22:12 -0400

    Animation: update data add width/height video

--- src/app/pages/animation/animation-project-data.ts
+++ src/app/pages/animation/animation-project-data.ts
@@ -5,150 +5,200 @@ export const ANIMATIONPROJECTS: AnimationProject[] = [
     imageUrl: 'union-force',
     title: 'L\'union fait la force, Radio-Canada',
     date: 2003,
-    color: 'rouge'
+    color: 'rouge',
+    width: 752,
+    height: 432
   },
   {
     imageUrl: 'auberge-chien-noir',
     title: 'Auberge du Chien Noir, Radio-Canada',
     date: 2003,
-    color: 'gris'
+    color: 'gris',
+    width: 704,
+    height: 416
   },
   {
     imageUrl: 'imax',
     title: 'IMAX',
     date: 1998,
-    color: 'brun'
+    color: 'brun',
+    width: 640,
+    height: 400
   },
   {
     imageUrl: 'soule-ton-boss',
     title: 'Soul ton Boss, Cyber Generation',
     date: 2002,
-    color: 'rouge'
+    color: 'rouge',
+    width: 784,
+    height: 440
   },
   {
     imageUrl: 'animation-flash',
     title: 'Animation Flash',
     date: 2008,
-    color: 'mauve'
+    color: 'mauve',
+    width: 960,
+    height: 592
   },
   {
     imageUrl: 'jeux-flash',
     title: 'Jeux Flash',
     date: 2008,
-    color: 'brun'
+    color: 'brun',
+    width: 960,
+    height: 592
   },
   {
     imageUrl: 'portfolio',
     title: 'Portfolio',
     date: 2008,
-    color: 'bleu'
+    color: 'bleu',
+    width: 960,
+    height: 592
   },
   {
     imageUrl: 'on-ne-ment-que-deux-fois',
     title: 'On ne ment que deux fois, Radio-Canada',
     date: 2003,
-    color: 'vert'
+    color: 'vert',
+    width: 752,
+    height: 416
   },
   {
     imageUrl: 'mikado',
     title: 'MIKADO bikes, Macromedia site of the day',
     date: 2002,
-    color: 'orange'
+    color: 'orange',
+    width: 776,
+    height: 432
   },
   {
     imageUrl: 'porte-jpr',
     title: 'Porte JPR Web Site',
     date: 2003,
-    color: 'bleu'
+    color: 'bleu',
+    width: 736,
+    height: 352
   },
   {
     imageUrl: 'centre-science',
     title: 'Centre des sciences de Montréal',
     date: 2000,
-    color: 'rouge'
+    color: 'rouge',
+    width: 632,
+    height: 416
   },
   {
     imageUrl: 'fraser-contract',
     title: 'Fraser Contract web site',
     date: 2002,
-    color: 'gris'
+    color: 'gris',
+    width: 680,
+    height: 416
   },
   {
-    imageUrl: 'dressing-room',
+    imageUrl: 'visual-search',
     title: 'Dressing Room application MVM',
     date: 2008,
-    color: 'rose'
+    color: 'rose',
+    width: 648,
+    height: 512
   },
   {
     imageUrl: 'print',
     title: 'Print projects',
     date: 2008,
-    color: 'rose'
+    color: 'rose',
+    width: 960,
+    height: 592
   },
   {
-    imageUrl: 'hm',
+    imageUrl: 'hm-application-prototype',
     title: 'H&M application prototype',
     date: 2007,
-    color: 'bleu'
+    color: 'bleu',
+    width: 752,
+    height: 544
   },
   {
     imageUrl: 'msn',
     title: 'MSN Dressing room application',
     date: 2007,
-    color: 'vert'
+    color: 'vert',
+    width: 768,
+    height: 560
   },
   {
     imageUrl: 'raid',
     title: 'Raid Web Site',
     date: 2001,
-    color: 'orange'
+    color: 'orange',
+    width: 776,
+    height: 400
   },
   {
     imageUrl: 'oryx-evolution',
     title: 'ORYX Bikes application',
     date: 2004,
-    color: 'gris'
+    color: 'gris',
+    width: 800,
+    height: 576
   },
   {
     imageUrl: 'reebok',
     title: 'Reebok Prototype',
     date: 2007,
-    color: 'rouge'
+    color: 'rouge',
+    width: 912,
+    height: 584
   },
   {
     imageUrl: 'carte-noel-mvm',
     title: 'Chrismas card MVM',
     date: 2005,
-    color: 'bleu'
+    color: 'bleu',
+    width: 728,
+    height: 272
   },
   {
     imageUrl: 'cerveau-direction',
     title: 'Cerveau Direction, Radio-Canada',
     date: 2000,
-    color: 'bleu'
+    color: 'bleu',
+    width: 752,
+    height: 456
   },
   {
     imageUrl: 'renovation-ram',
     title: 'Renovation Ram Web Site',
     date: 2003,
-    color: 'orange'
+    color: 'orange',
+    width: 776,
+    height: 424
   },
   {
     imageUrl: 'oryx-evolution-builder',
     title: 'ORYX Bikes application',
     date: 2003,
-    color: 'rouge'
+    color: 'rouge',
+    width: 768,
+    height: 448
   },
   {
     imageUrl: 'radio-canada-50',
     title: 'Radio-Canada 50e anniversaire',
     date: 2002,
-    color: 'gris'
+    color: 'gris',
+    width: 712,
+    height: 400
   },
   {
     imageUrl: 'sears-usa',
     title: 'Sears Application prototype',
     date: 2008,
-    color: 'bleu'
+    color: 'bleu',
+    width: 752,
+    height: 624
   }
 ];
