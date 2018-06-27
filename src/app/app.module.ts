9b84fa57c46c189d66848fe1d0f27676b1cb5263
Alexandre Payette <apayette@videotron.ca>
2018-06-26 21:05:06 -0400

    app.module: useHash: true / define Routes / import component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { WebDesignModule } from './pages/web-design/web-design.module';
 import { PhotoModule } from './pages/photo/photo.module';
 import { AnimationModule } from './pages/animation/animation.module';
@@ -9,16 +9,29 @@ import { AnimationModule } from './pages/animation/animation.module';
 import { AppComponent } from './app.component';
 
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { WebDesignComponent } from './pages/web-design/web-design.component';
+import { AnimationComponent } from './pages/animation/animation.component';
+import { PhotoComponent } from './pages/photo/photo.component';
 import { TechnicalSkillsComponent } from './pages/technical-skills/technical-skills.component';
 import { ExperienceComponent } from './pages/experience/experience.component';
 import { ContactComponent } from './pages/contact/contact.component';
-
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { WebDesignService } from './pages/web-design/web-design.service';
 import { PhotoService } from './pages/photo/photo.service';
 import { AnimationService } from './pages/animation/animation.service';
+
+const routes: Routes = [
+  { path: '', component: WebDesignComponent },
+  { path: 'animation', component: AnimationComponent },
+  { path: 'photo', component: PhotoComponent },
+  { path: 'technical-skills', component: TechnicalSkillsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   declarations: [
@@ -32,7 +45,7 @@ import { AnimationService } from './pages/animation/animation.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes, { useHash: true }),
     HttpClientModule,
     WebDesignModule,
     PhotoModule,
