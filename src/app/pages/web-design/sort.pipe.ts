2f8a623490c3a5ac2a80356b6d7ec5aa16add62b
Alexandre Payette <apayette@videotron.ca>
2018-08-27 21:15:03 -0400

    sort pipe: improve code - use array.reverse()

--- src/app/pages/web-design/sort.pipe.ts
+++ src/app/pages/web-design/sort.pipe.ts
@@ -4,28 +4,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'sort'
 })
 export class ArraySortPipe implements PipeTransform {
-  transform(array: any[], field: string, propName: boolean): any[] {
-    if (propName === true) {
-      array.sort((a: any, b: any) => {
-        if (a[field] > b[field]) {
+  transform(array: any[], property: string, reverse: boolean): any[] {
+    function sortOn(prop) {
+      return function(a, b){
+        if (a[prop] < b[prop]) {
           return -1;
-        } else if (a[field] < b[field]) {
+        } else if (a[prop] > b[prop]) {
           return 1;
         } else {
           return 0;
         }
-      });
-    } else {
-      array.sort((a: any, b: any) => {
-        if (a[field] < b[field]) {
-          return -1;
-        } else if (a[field] > b[field]) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
+      };
     }
-    return array;
+
+    if (reverse === false) {
+      return array.sort(sortOn(property));
+    } else {
+      return array.sort(sortOn(property)).reverse();
+    }
   }
 }
