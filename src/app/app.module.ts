c0f9dd2f393aa17cc42f8caeeabf95126a2d5c4c
Alexandre Payette <apayette@videotron.ca>
2018-11-11 17:19:55 -0500

    Create Contact Module / import ReactiveFormsModule & ContactComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { WebDesignModule } from './pages/web-design/web-design.module';
 import { PhotoModule } from './pages/photo/photo.module';
 import { AnimationModule } from './pages/animation/animation.module';
+import { ContactModule } from './pages/contact/contact.module';
 
 import { AppComponent } from './app.component';
 
 import { HeaderNavComponent } from './header-nav/header-nav.component';
 import { TechnicalSkillsComponent } from './pages/technical-skills/technical-skills.component';
 import { ExperienceComponent } from './pages/experience/experience.component';
-import { ContactComponent } from './pages/contact/contact.component';
 
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { FooterComponent } from './footer/footer.component';
@@ -26,7 +26,6 @@ import { AnimationService } from './pages/animation/animation.service';
     HeaderNavComponent,
     TechnicalSkillsComponent,
     ExperienceComponent,
-    ContactComponent,
     PageNotFoundComponent,
     FooterComponent
   ],
@@ -36,7 +35,8 @@ import { AnimationService } from './pages/animation/animation.service';
     HttpClientModule,
     WebDesignModule,
     PhotoModule,
-    AnimationModule
+    AnimationModule,
+    ContactModule
   ],
   providers: [
     WebDesignService,
