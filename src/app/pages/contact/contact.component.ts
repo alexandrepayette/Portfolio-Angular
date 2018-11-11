64db915cccd118814bf64eca8b7f3a2db9ea6864
Alexandre Payette <apayette@videotron.ca>
2018-11-11 17:21:22 -0500

    Contact.ts: front-end validation / add regex email & comment

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  public isEmailInput = false;
+  public contactForm: FormGroup;
+  private emailRegEx = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  private commentRegEx = /^[^<>|`~%*[\]{}\^]+$/;
 
   ngOnInit() {
+    this.contactForm = new FormGroup({
+      'firstName': new FormControl(null, [Validators.required]),
+      'lastName': new FormControl(null, [Validators.required]),
+      'email': new FormControl(null, [Validators.required, Validators.pattern(this.emailRegEx)]),
+      'comment': new FormControl(null, [Validators.required, Validators.pattern(this.commentRegEx)]),
+    });
   }
 
+  eventEmailFocus() {
+    this.isEmailInput = true;
+  }
+
+  eventEmailBlur() {
+    this.isEmailInput = false;
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.contactForm.value);
+  }
 }
